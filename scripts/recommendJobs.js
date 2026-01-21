9f7073f634be5b840d6a286a29332de7fc57ae96
mdkim <4809449+mdkim@users.noreply.github.com>
2026-01-21 01:01:57 -0800

    function decomposition in reommendJobs.js

--- scripts/recommendJobs.js
+++ scripts/recommendJobs.js
@@ -34,17 +34,17 @@ async function main() {
     sendStatusToPopup(`Done. Opened ${result.length} jobs tabs`, '', 'recommend_done');
 }
 
-function sendOpenCompanyJobsToPopup(companyUrl, injectedDivHTML, cbPopupOpen) {
+function sendOpenCompanyJobsToPopup(companyUrl, injectedDivHTML, callbackDoOpen) {
     chrome.runtime.sendMessage({
         action: 'open_company_jobs', companyUrl, injectedDivHTML
     }, () => {
         const lastError = chrome.runtime.lastError;
         if (lastError) {
             debug(`\`sendOpenCompanyJobsToPopup()\` Popup is closed: ${lastError.message}`);
-            cbPopupOpen(false);
+            callbackDoOpen(false);
             return;
         }
-        cbPopupOpen(true);
+        callbackDoOpen(true);
     });
 };
 
@@ -54,21 +54,7 @@ async function mainRecommend() {
     window.__extInjectedObserverConnected = false;
     document.getElementById('ext-injected')?.remove();
 
-    let topDivs, paragraphsType;
-    {
-        // for '/search/', '/collections/'
-        topDivs = document.querySelectorAll(
-            'li.scaffold-layout__list-item .job-card-container--clickable'
-        );
-        paragraphsType = 'span';
-    }
-    if (topDivs.length === 0) {
-        // for '/search-results/'
-        topDivs = document.querySelectorAll(
-            '[data-view-name="job-search-job-card"] [role="button"]'
-        );
-        paragraphsType = 'p';
-    }
+    let { topDivs, paragraphsType } = getTopDivs();
     if (topDivs.length === 0) {
         return [];
     }
@@ -99,21 +85,7 @@ async function mainRecommend() {
             if (!isStarted) isStarted = true;
         }
 
-        const paragraphs = [...topDiv.querySelectorAll(paragraphsType)]
-            .filter(p => !p.classList.contains('visually-hidden')) // for '/search/', '/collections/' only
-            .map(p => {
-                const span = p.querySelector(':scope > span[aria-hidden="true"]')
-                // p.querySelector(':scope > span:not([aria-hidden])') // (Verified job)
-                return span
-                    ? span.childNodes[0].textContent.trim() // jobTitle
-                    : p.textContent.trim();
-            })
-            .filter(p => p !== '');
-
-        const jobCardPre = `Job card #${index + 1}:`;
-        debug(`Extracting, ${jobCardPre}`);
-        const jobCard = extractJobCard(paragraphs);
-        debug(jobCard);
+        const { jobCard, jobCardPre } = getJobCard(index, topDiv, paragraphsType);
 
         if (jobCard.status === "Saved") {
             debug(`${jobCardPre} "Saved", skipping...`);
@@ -138,96 +110,9 @@ async function mainRecommend() {
         debug("Job card click dispatched");
 
         // "About the job" span from job panel
-        const currSpan = await spanFromStableJobPanel()
-            .catch((err) => {
-                sendStatusToPopup(err.message, 'error');
-                throw err;
-            });
-        const aboutTheJobText = currSpan.innerText.trim();
+        const { companyUrl, textHighlights } = await getJobInfo(jobCard, companiesDone, results);
 
-        results.push(jobCard);
-        companiesDone.add(jobCard.company);
-
-        // company URL from job panel
-        const companyUrl = (
-            // for '/search/', '/collections/'
-            document.querySelector('.job-details-jobs-unified-top-card__company-name a[href]')
-            // for '/search-results/'
-            ?? document.querySelector('[data-testid="lazy-column"] a[href*="linkedin.com/company/"]')
-        )
-            .href.replace(/\/[\w-]+\/?$/, '/jobs/');
-
-        // "About the job" higlights:
-        const keywordRegex = new RegExp(`\\b(${CONFIG.HIGHLIGHT_SKILLS.join('|')})`, 'gi');
-
-        const textHighlights = aboutTheJobText
-            .split(/\n|\.\s+/)
-            .filter(sentence => {
-                keywordRegex.lastIndex = 0; // reset regex for 'g' flag
-                return keywordRegex.test(sentence);
-            })
-            .map(s => {
-                const highlighted = s.trim().replace(keywordRegex, '<span class="ext-highlight">$1</span>');
-                return `• ${highlighted}`;
-            })
-            .join("\n<br>\n");
-
-        let injectedDivHTML =
-            `<div id="ext-injected" class="artdeco-entity-lockup--size-5">
-    <style>
-    #ext-injected {
-        border: 2px solid #AA6C39;
-        border-radius: 12px;
-        padding: 12px 24px 22px 24px;
-        margin: 0 24px 24px 24px;
-    }
-    .ext-highlight {
-        font-weight: 700;
-        text-decoration: underline;
-        color: #000;
-    }
-    hr.ext-hr {
-        margin: 14px 0px 10px 0px;
-        border-top: 2px solid;
-        border-color: #777;
-    }
-    .ext-font-caption {
-        font-weight: 360;
-    }
-    </style>
-    <span class="artdeco-entity-lockup__caption"><em>Opened from</em></span>
-    <span class="artdeco-entity-lockup__title"><strong>${jobCard.jobTitle}</strong></span>
-    <br>
-    <span class="artdeco-entity-lockup__subtitle">${jobCard.location}</span>`;
-
-        const { exportedJobs } = await chrome.storage.local.get('exportedJobs');
-        const savedJobsFromCompanyText = exportedJobs
-            .filter(job => job[1] === jobCard.company)
-            .map(job => `• ${job[3]}, <em>(${job[2]})</em>`) // title, location
-            .join("\n<br>\n");
-        if (savedJobsFromCompanyText) {
-            injectedDivHTML += `
-    <hr class="ext-hr">
-    <span class="artdeco-entity-lockup__title" style="font-size: 1.14em">
-        Saved Jobs&nbsp;<em class="ext-font-caption">from</em>&nbsp;${jobCard.company}
-        <br>
-    </span>
-    <span class="artdeco-entity-lockup__caption">
-    ${savedJobsFromCompanyText}
-    </span>`;
-        }
-
-        injectedDivHTML += `
-    <hr class="ext-hr">
-    <span class="artdeco-entity-lockup__title" style="font-size: 1.14em">
-        About the job&nbsp;<em class="ext-font-caption"></em>
-        <br>
-    </span>
-    <span class="artdeco-entity-lockup__caption">
-    ${textHighlights}
-    </span>
-</div>
-`;
+        let injectedDivHTML = await getInjectedDivHTML(jobCard, textHighlights);
 
         sendOpenCompanyJobsToPopup(companyUrl, injectedDivHTML, isPopupOpen => {
             if (!isPopupOpen) isPopupClosed = true;
@@ -238,6 +123,105 @@ async function mainRecommend() {
     }
 
     return results;
+}
+
+function getTopDivs() {
+    let topDivs, paragraphsType;
+    {
+        // for '/search/', '/collections/'
+        topDivs = document.querySelectorAll(
+            'li.scaffold-layout__list-item .job-card-container--clickable'
+        );
+        paragraphsType = 'span';
+    }
+    if (topDivs.length === 0) {
+        // for '/search-results/'
+        topDivs = document.querySelectorAll(
+            '[data-view-name="job-search-job-card"] [role="button"]'
+        );
+        paragraphsType = 'p';
+    }
+    return { topDivs, paragraphsType };
+}
+
+function getJobCard(index, topDiv, paragraphsType) {
+    const paragraphs = [...topDiv.querySelectorAll(paragraphsType)]
+        .filter(p => !p.classList.contains('visually-hidden')) // for '/search/', '/collections/' only
+        .map(p => {
+            const span = p.querySelector(':scope > span[aria-hidden="true"]');
+            // p.querySelector(':scope > span:not([aria-hidden])') // (Verified job)
+            return span
+                ? span.childNodes[0].textContent.trim() // jobTitle
+                : p.textContent.trim();
+        })
+        .filter(p => p !== '');
+
+    const jobCardPre = `Job card #${index + 1}:`;
+    debug(`Extracting, ${jobCardPre}`);
+    const jobCard = extractJobCard(paragraphs);
+    debug(jobCard);
+    return { jobCard, jobCardPre };
+}
+
+function extractJobCard(paragraphs) {
+    const result = {
+        jobTitle: paragraphs[0],
+        company: paragraphs[1],
+        location: paragraphs[2],
+        status: null,
+        companyAlumni: null,
+        schoolAlumni: null
+    };
+
+    for (const text of paragraphs.slice(3)) {
+        if (text === "Viewed" || text === "Saved") {
+            result.status = text;
+        } else if (text.includes('alumni work')) {
+            if (text.includes('company alumni')) {
+                result.companyAlumni = text;
+            } else if (text.includes('school alumni')) {
+                result.schoolAlumni = text;
+            }
+        }
+    }
+    return result;
+}
+
+async function getJobInfo(jobCard, companiesDone, results) {
+    const currSpan = await spanFromStableJobPanel()
+        .catch((err) => {
+            sendStatusToPopup(err.message, 'error');
+            throw err;
+        });
+    const aboutTheJobText = currSpan.innerText.trim();
+
+    results.push(jobCard);
+    companiesDone.add(jobCard.company);
+
+    // company URL from job panel
+    const companyUrl = (
+        // for '/search/', '/collections/'
+        document.querySelector('.job-details-jobs-unified-top-card__company-name a[href]')
+        // for '/search-results/'
+        ?? document.querySelector('[data-testid="lazy-column"] a[href*="linkedin.com/company/"]')
+    )
+        .href.replace(/\/[\w-]+\/?$/, '/jobs/');
+
+    // "About the job" higlights:
+    const keywordRegex = new RegExp(`\\b(${CONFIG.HIGHLIGHT_SKILLS.join('|')})`, 'gi');
+
+    const textHighlights = aboutTheJobText
+        .split(/\n|\.\s+/)
+        .filter(sentence => {
+            keywordRegex.lastIndex = 0; // reset regex for 'g' flag
+            return keywordRegex.test(sentence);
+        })
+        .map(s => {
+            const highlighted = s.trim().replace(keywordRegex, '<span class="ext-highlight">$1</span>');
+            return `• ${highlighted}`;
+        })
+        .join("\n<br>\n");
+    return { companyUrl, textHighlights };
 }
 
 async function spanFromStableJobPanel({
@@ -277,28 +261,63 @@ async function spanFromStableJobPanel({
     }
 }
 
-function extractJobCard(paragraphs) {
-    const result = {
-        jobTitle: paragraphs[0],
-        company: paragraphs[1],
-        location: paragraphs[2],
-        status: null,
-        companyAlumni: null,
-        schoolAlumni: null
-    };
-
-    for (const text of paragraphs.slice(3)) {
-        if (text === "Viewed" || text === "Saved") {
-            result.status = text;
-        } else if (text.includes('alumni work')) {
-            if (text.includes('company alumni')) {
-                result.companyAlumni = text;
-            } else if (text.includes('school alumni')) {
-                result.schoolAlumni = text;
-            }
-        }
+async function getInjectedDivHTML(jobCard, textHighlights) {
+    let injectedDivHTML = `<div id="ext-injected" class="artdeco-entity-lockup--size-5">
+    <style>
+    #ext-injected {
+        border: 2px solid #AA6C39;
+        border-radius: 12px;
+        padding: 12px 24px 22px 24px;
+        margin: 0 24px 24px 24px;
     }
-    return result;
+    .ext-highlight {
+        font-weight: 700;
+        text-decoration: underline;
+        color: #000;
+    }
+    hr.ext-hr {
+        margin: 14px 0px 10px 0px;
+        border-top: 2px solid;
+        border-color: #777;
+    }
+    .ext-font-caption {
+        font-weight: 360;
+    }
+    </style>
+    <span class="artdeco-entity-lockup__caption"><em>Opened from</em></span>
+    <span class="artdeco-entity-lockup__title"><strong>${jobCard.jobTitle}</strong></span>
+    <br>
+    <span class="artdeco-entity-lockup__subtitle">${jobCard.location}</span>`;
+
+    const { exportedJobs } = await chrome.storage.local.get('exportedJobs');
+    const savedJobsFromCompanyText = exportedJobs
+        .filter(job => job[1] === jobCard.company)
+        .map(job => `• ${job[3]}, <em>(${job[2]})</em>`) // title, location
+        .join("\n<br>\n");
+    if (savedJobsFromCompanyText) {
+        injectedDivHTML += `
+    <hr class="ext-hr">
+    <span class="artdeco-entity-lockup__title" style="font-size: 1.14em">
+        Saved Jobs&nbsp;<em class="ext-font-caption">from</em>&nbsp;${jobCard.company}
+        <br>
+    </span>
+    <span class="artdeco-entity-lockup__caption">
+    ${savedJobsFromCompanyText}
+    </span>`;
+    }
+
+    injectedDivHTML += `
+    <hr class="ext-hr">
+    <span class="artdeco-entity-lockup__title" style="font-size: 1.14em">
+        About the job&nbsp;<em class="ext-font-caption"></em>
+        <br>
+    </span>
+    <span class="artdeco-entity-lockup__caption">
+    ${textHighlights}
+    </span>
+</div>
+`;
+    return injectedDivHTML;
 }
 
 // end IIFE
